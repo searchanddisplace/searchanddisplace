d01fc073e48a9491967c21caf325d24552e43db1
Lucian Pricop <lucian@newro.co>
2020-04-25 16:50:44 +0300

    more nix improvements

--- default.nix
+++
@@ -1,9 +0,0 @@
-{ system ? builtins.currentSystem }:
-let
-  pkgs = import <nixpkgs> { inherit system; };
-  #self = in self
-in rec {
-    search-and-displace = pkgs.callPackage ./pkgs/sandd.nix {
-      inherit pkgs;
-    };
-}
